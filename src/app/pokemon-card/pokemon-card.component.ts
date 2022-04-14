2c040bc60a1653cec0cdec454dbf11d8b235830a
acastron22 <arielcastron22@hotmail.com>
2022-04-14 15:15:16 -0300

    Dados de mais pokemons aparecendo para cada item

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './../../_model/Pokemon';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.css']
+  styleUrls: ['./pokemon-card.component.css'],
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
+  @Input()
+  public 'pokemon': Pokemon;
 
-  constructor() { }
+  public leadingZero(str: string | number, size: number = 3): string {
+    let s = String(str);
 
-  ngOnInit(): void {
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
+import { Type } from 'src/_model/Type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnInit {
+  public pokemons: Pokemon[] = [
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbassaur',
+      types: [Type.Grass, Type.Poison],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [Type.Grass, Type.Poison],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: 'Venossaur',
+      types: [Type.Grass, Type.Poison],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
